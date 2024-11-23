c8dfaa4061f8c202b0ec2b1ca95c327bcc374406
Steven Barquet <mlmjm123@gmail.com>
2024-11-23 07:29:47 -0600

    frontend config working

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      '@node_modules': './node_modules',
+      '@node_modules': '../../node_modules',
       '@src': './src',
     },
   },
